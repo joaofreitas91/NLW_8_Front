7ada26b79a5fa43917c2dd1f586eacad9bc2a20d
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2022-05-03 23:28:32 -0300

    Widget Form Nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
         extend: {
             colors: {
                 brand: {
+                    300: "#996dff",
                     500: "#8257e6",
                 },
             },
+            borderRadius: {
+                md: "0.250rem" /* 4px */,
+            },
         },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
