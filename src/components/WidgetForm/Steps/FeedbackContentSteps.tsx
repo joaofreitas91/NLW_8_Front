538aa2b9f92cf53c72c06b9cff16945fc50a0871
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2022-05-04 22:39:40 -0300

    SendFeedback

--- src/components/WidgetForm/Steps/FeedbackContentSteps.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentSteps.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from ".";
 import { CloseButton } from "../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -7,14 +7,22 @@ import { ScreenshotButton } from "../ScreenshotButton";
 interface FeedbackContentProps {
     feedbackType: FeedbackType;
     onFeedbackRestart: () => void;
+    onFeedbackSent: () => void;
 }
 
 export function FeedbackContentSteps({
     feedbackType,
     onFeedbackRestart,
+    onFeedbackSent,
 }: FeedbackContentProps) {
     const [screenShot, setScreenShot] = useState<string | null>(null);
+    const [comment, setComment] = useState("");
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        onFeedbackSent();
+    }
 
     return (
         <>
@@ -36,10 +44,15 @@ export function FeedbackContentSteps({
                 </span>
                 <CloseButton />
             </header>
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
                     className=" min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-2 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin pr-3"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onInput={(event: FormEvent) => {
+                        const { target } = event;
+                        if (target)
+                            setComment((target as HTMLButtonElement).value);
+                    }}
                 />
                 <footer className="flex gap-2 mt-2">
                     <ScreenshotButton
@@ -48,7 +61,8 @@ export function FeedbackContentSteps({
                     />
                     <button
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        disabled={!comment && true}
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
                         Enviar Feedback
                     </button>
